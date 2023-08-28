b175991c703a8e28e5695465e1f3dd11abdd4d0a
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-08-28 20:52:31 +0200

    [ADD] nginx -> [PROB] dockerfile CMD ["nginx", "-g", "daemon off;"] + [ADD] mariadb should be good, missing wordpress to test it

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
+
+
+# touch file
+# chmod 777 file
+
+# echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> file
+# echo "FLUSH PRIVILEGES;" >> file
+# echo "GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> file
+# echo "FLUSH PRIVILEGES;" >> file
+# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> file
+# echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"  >> file
+# echo "FLUSH PRIVILEGES;" >> file
+
+# # cat file
+
+# mysqld --user=mysql --verbose --bootstrap < file
+# rm file
+
+# exec mysqld
