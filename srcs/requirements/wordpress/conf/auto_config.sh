dd62714498b254385be09de399d8de0bbd9651b7
Manuel <manuel.sharifi@gmail.com>
2023-08-30 17:52:53 +0200

    [FIX] path to volume local save

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 sleep 10
-test -f /etc/conf/php.conf && wp config create	--allow-root \
-												--dbname=$SQL_DATABASE \
-												--dbuser=$SQL_USER \
-												--dbpass=$SQL_PASSWORD \
-												--dbhost=mariadb:3306 --path='/var/www/wordpress' \
-							&&  wp core install --url="msharifi.42.fr" --title="Inception" \
-							--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
-							--admin_email=$WP_ADMIN_MAIL --allow-root
-							wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASS --allow-root
-							wp config shuffle-salts --allow-root
-							echo "Wordpress's installation complete"
+
+if ! wp core is-installed --allow-root  ; then
+    wp core download --allow-root --force
+    wp config create --dbname=wordpress --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD --dbhost=mariadb \
+    --allow-root --force
+    wp core install --url="sleon.42.fr" --title="Inception" \
+    --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
+    --admin_email=$WP_ADMIN_MAIL --allow-root
+    wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASS --allow-root
+    wp config shuffle-salts --allow-root
+    echo "Wordpress's installation complete"
+fi
 
 if wp core is-installed --allow-root  ; then
-	echo "Wordpress is installed and running"
-	php-fpm7.3 -F -R
+    echo "Wordpress is installed and running"
+    php-fpm7.3 -F -R
 else
-	echo "Wordpress's installation failed"
+    echo "Wordpress's installation failed"
 fi
